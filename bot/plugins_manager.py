b9fe529977bc1966d1c0977cbe500e38bbbe2647
Nikita L <hexlite24@gmail.com>
2026-05-31 23:43:51 +0200

    chore: some improvements

--- bot/plugins_manager.py
+++ bot/plugins_manager.py
@@ -1,5 +1,7 @@
+import logging
 import os
 from importlib import import_module
+from typing import Iterator
 
 from aiogram import Router
 from models import PluginModel
@@ -7,18 +9,28 @@ from services.savmes.actions import MessageActions
 from settings import PLUGINS_DIR_PATH, PLUGINS_MODULE_NAME
 
 
+logger = logging.getLogger(__name__)
+
+
 class PluginsManager:
     def __init__(self) -> None:
         self._plugins: list[PluginModel] = []
+
         for plugin_name in os.listdir(PLUGINS_DIR_PATH):
             try:
                 plugin_module = import_module(f"{PLUGINS_MODULE_NAME}.{plugin_name}")
             except ModuleNotFoundError:
                 continue
 
-            self._plugins.append(self._load_plugin(plugin_module))
+            if plugin := self._load_plugin(plugin_module):
+                self._plugins.append(plugin)
 
         self._load_actions()
+
+        if self._plugins:
+            logger.info("Loaded plugins: %s", ", ".join(str(v) for v in self._plugins))
+        else:
+            logger.info("No plugins loaded")
 
     def _load_actions(self) -> None:
         loaded_actions = []
@@ -26,7 +38,14 @@ class PluginsManager:
             loaded_actions.extend(plugin.actions)
         MessageActions.load_custom_actions(loaded_actions)
 
-    def _load_plugin(self, module) -> PluginModel:
+    def _load_plugin(self, module) -> PluginModel | None:
+
+        try:
+            if not module.ENABLED:
+                return None
+        except AttributeError:
+            pass
+
         try:
             commands_router = module.commands_router
         except AttributeError:
@@ -42,12 +61,15 @@ class PluginsManager:
         except AttributeError:
             tasks = ()
 
+        if not any((tasks, commands_router, actions)):
+            return None
+
         return PluginModel(commands_router=commands_router, tasks=tasks, actions=actions)
 
-    def get_commands_routers(self) -> tuple[Router]:
+    def get_commands_routers(self) -> Iterator[Router]:
         return (v.commands_router for v in self._plugins if v.commands_router is not None)
 
-    def load_tasks(self, app) -> list[Router]:
+    def load_tasks(self, app) -> None:
         tasks = []
         for plugin in self._plugins:
             tasks.extend(plugin.tasks)
