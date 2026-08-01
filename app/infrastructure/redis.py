94eb2cb07f7de8bd38a44f133bb069da678bc6a9
Nikita L <hexlite24@gmail.com>
2026-08-01 18:12:07 +0200

    chore: decode redis responses

--- app/infrastructure/redis.py
+++ app/infrastructure/redis.py
@@ -4,4 +4,4 @@ from .settings import infrastructure_settings
 
 
 async def make_redis_client() -> aioredis.Redis:
-    return await aioredis.from_url(infrastructure_settings.redis_client_url)
+    return await aioredis.from_url(infrastructure_settings.redis_client_url, decode_responses=True)
