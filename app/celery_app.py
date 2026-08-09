55da0335c3be7774c442c32af6d7e08529883409
Nikita L <hexlite24@gmail.com>
2026-08-09 18:02:02 +0200

    chore: improve celery loading

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,21 +1,13 @@
 from typing import Any
-from celery import Celery, Task
+from celery import Celery as CeleryApp
 
 from app.plugins_manager import plugins_manager
 from app.infrastructure import infrastructure_settings
 
 
-class TestTask(Task):
-    def run(self, data, msgdoc):
-        print(data)
-        return data
-
-
-class CeleryApp(Celery):
-    def __init__(self, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-
-        plugins_manager.load_tasks(self)
-
-
 app = CeleryApp("tasks", broker=infrastructure_settings.celery_broker, backend=infrastructure_settings.celery_backend)
+
+
+@app.on_after_configure.connect
+def setup_app_context(sender: CeleryApp, **_: Any) -> None:
+    plugins_manager.load_tasks(sender)
